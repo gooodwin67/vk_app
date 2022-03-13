917a7ebbadb29e64e721ef535765793745a4954a
gooodwin67 <sn67@inbox.ru>
2022-03-13 15:36:20 +0300

    peoples user profile

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:vk_app/widgets/login/register_user.dart';
 import 'package:vk_app/widgets/login/confirm-send.dart';
 import 'package:vk_app/widgets/login/reset_password.dart';
 import 'package:vk_app/widgets/main/main_screen.dart';
+import 'package:vk_app/widgets/profiles/peoples_profile.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,10 +17,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       //home: LoginScreenWidget(),
-      initialRoute: '/',
+      initialRoute: '/main',
       routes: {
         '/': (context) => LoginScreenWidget(),
         '/main': (context) => MainScreenWidget(),
+        '/main/people-profile': (context) => PeopleProfileWidget(),
         '/reset-password': (context) => ResetPasswordScreenWidget(),
         '/register': (context) => RegisterUserWidget(),
         '/confirm-send': (context) => ConfirmSendWidget(),
