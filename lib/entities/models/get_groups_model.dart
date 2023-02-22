b7cefba85ec68ed87eb3e76e79d61edf41428eb3
Light Course <sn67@inbox.ru>
2023-02-22 16:35:40 +0300

    groups

--- lib/entities/models/get_groups_model.dart
+++ lib/entities/models/get_groups_model.dart
@@ -1,16 +1,31 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 
 import 'package:provider/provider.dart';
 import 'package:vk_app/domain/api_client/api_client.dart';
+import 'package:vk_app/entities/get_groups_entity.dart';
 
 class GetGroupsModel extends ChangeNotifier {
+  List _items = [];
+  int _countGroups = 0;
+
+  List get items => _items;
+  int get countGroups => _countGroups;
+
   Future getGroups(BuildContext context, text, offset, count) async {
     final token = context.read<ApiClient>().token;
 
     var getGroups = await http.get(Uri.parse(
         'https://api.vk.com/method/groups.search?v=5.131&access_token=$token&q=$text&offset=$offset&count=$count'));
 
-    var aa = getGroups.body;
+    final response = Response.fromJson(jsonDecode(getGroups.body)).response;
+    if (response != null) {
+      _countGroups = response['count'];
+      _items = response['items'].map((e) => Item.fromJson(e)).toList();
+    }
+
+    notifyListeners();
   }
 }
--- lib/widgets/menu-screens/groups-screen/groups-screen.dart
+++ lib/widgets/menu-screens/groups-screen/groups-screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:vk_app/constants/constants.dart';
 import 'package:vk_app/entities/models/get_groups_model.dart';
 
 class GroupsScreenWidget extends StatefulWidget {
@@ -11,15 +12,179 @@ class GroupsScreenWidget extends StatefulWidget {
 
 class _GroupsScreenWidgetState extends State<GroupsScreenWidget> {
   @override
-  void didChangeDependencies() {
-    context.read<GetGroupsModel>().getGroups(context, 'Челси', 0, 0);
-    super.didChangeDependencies();
+  void initState() {
+    context.read<GetGroupsModel>().getGroups(context, 'а', 0, 10);
+    print(123);
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Groups'),
+    var countGroups = context.watch<GetGroupsModel>().countGroups;
+    var items = context.watch<GetGroupsModel>().items;
+
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.only(
+            left: constants.mainPadding,
+            right: constants.mainPadding,
+            top: constants.mainPadding,
+          ),
+          child: CustomScrollView(
+            slivers: [
+              SliverAppBar(
+                backgroundColor: Colors.white,
+                foregroundColor: Colors.black,
+                automaticallyImplyLeading: false,
+                titleSpacing: 0,
+                floating: false,
+                title: Row(
+                  children: [
+                    Expanded(
+                      child: TextField(
+                        onChanged: (value) {
+                          context
+                              .read<GetGroupsModel>()
+                              .getGroups(context, value, 0, 10);
+                        },
+                        decoration: InputDecoration(
+                          label: Text('Поиск'),
+                          floatingLabelBehavior: FloatingLabelBehavior.never,
+                          prefixIcon: Icon(Icons.search),
+                          filled: true,
+                          fillColor: constants.backColor,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide: BorderSide.none,
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: 10),
+                    InkWell(
+                      child: Icon(Icons.format_align_left_sharp),
+                      onTap: () => showDialog(
+                        context: context,
+                        builder: (context) {
+                          return SingleChildScrollView(
+                            child: AlertDialog(
+                              insetPadding: EdgeInsets.all(5),
+                              contentPadding: EdgeInsets.all(15),
+                              titlePadding:
+                                  EdgeInsets.all(15).copyWith(bottom: 0),
+                              title: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: const [
+                                  Text(
+                                    'Фильтры',
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.bold),
+                                  ),
+                                ],
+                              ),
+                              content: Text('data'),
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SliverList(
+                delegate: SliverChildBuilderDelegate(
+                    (context, index) => Container(
+                          margin: EdgeInsets.only(top: 20),
+                          child: Column(
+                            children: [
+                              Row(
+                                children: [
+                                  Text(
+                                    'Сообщества',
+                                    style: TextStyle(
+                                        fontSize: 17,
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.black),
+                                  ),
+                                ],
+                              ),
+                              SizedBox(height: 10),
+                              Divider(
+                                color: constants.secondColor,
+                              ),
+                            ],
+                          ),
+                        ),
+                    childCount: 1),
+              ),
+              SliverList(
+                delegate: SliverChildBuilderDelegate(
+                  (context, index) {
+                    // if (index == items.length - 1) {
+                    //   context.read<SearchScreenModel>().getUsersSearch(context);
+                    // }
+                    return Container(
+                      margin: EdgeInsets.only(bottom: constants.mainPadding),
+                      child: Row(
+                        children: [
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(50),
+                            child: Container(
+                              width: 50,
+                              height: 50,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(50),
+                              ),
+                              child: FadeInImage(
+                                fit: BoxFit.cover,
+                                image: NetworkImage(
+                                    items[index].photo100.toString()),
+                                placeholder: const AssetImage(
+                                    'assets/images/loading.gif'),
+                                imageErrorBuilder:
+                                    (context, error, stackTrace) {
+                                  print(error); //do something
+                                  return Image.asset(
+                                      'assets/images/no-avatar.png');
+                                },
+                              ),
+                            ),
+                          ),
+                          SizedBox(width: 10),
+                          Container(
+                            width: MediaQuery.of(context).size.width / 1.7,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  '${items[index].name}',
+                                  style: TextStyle(fontSize: 17),
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                                SizedBox(height: 5),
+                                Text(items[index].screenName.toString(),
+                                    style: TextStyle(fontSize: 12)),
+                              ],
+                            ),
+                          ),
+                          Spacer(),
+                          Icon(
+                            Icons.add,
+                            color: constants.mainColor,
+                          )
+                        ],
+                      ),
+                    );
+                  },
+                  childCount: items.length,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
